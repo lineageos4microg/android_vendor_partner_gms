ec738b982456570c29bd58ad849623609d2aa5be
Philip Nagler-Frank <philip@nagler.world>
2021-12-13 15:40:59 +0100

    Add product makefiles for proper integration with LineageOS build

---
+++ products/gms.mk
@@ -0,0 +1,10 @@
+PRODUCT_PACKAGES += \
+	GmsCore \
+	GsfProxy \
+	FakeStore \
+	MozillaNlpBackend \
+	NominatimNlpBackend \
+	com.google.android.maps \
+	FDroid \
+	FDroidPrivilegedExtension \
+	additional_repos.xml
---
+++ products/gms_go_2gb.mk
@@ -0,0 +1,1 @@
+-include $(LOCAL_PATH)/gms.mk
