881962077efaa799cb879df19639497558b13f75
danielk43 <11856117+danielk43@users.noreply.github.com>
2022-10-21 12:44:50 -0400

    Add optional local libs to GmsCore

--- GmsCore/Android.mk
+++ GmsCore/Android.mk
@@ -32,5 +32,6 @@ LOCAL_OVERRIDES_PACKAGES := com.qualcomm.location
 LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.gms.xml default-permissions-com.google.android.gms.xml sysconfig-com.google.android.gms.xml
 LOCAL_PRODUCT_MODULE := true
 LOCAL_USES_LIBRARIES := com.android.location.provider
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 include $(BUILD_PREBUILT)
 
