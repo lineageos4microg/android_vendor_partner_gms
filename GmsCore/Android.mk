0ec7fb7bccae82cb66582ceb14c59c3620fb309f
Pete Fotheringham <petefoth@e.email>
2022-12-17 08:34:13 +0000

    Use the `math_gt_or_eq` conditional to fix pre-19.1 builds

--- GmsCore/Android.mk
+++ GmsCore/Android.mk
@@ -30,8 +30,10 @@ LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_OVERRIDES_PACKAGES := com.qualcomm.location
 LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.gms.xml default-permissions-com.google.android.gms.xml sysconfig-com.google.android.gms.xml
-LOCAL_PRODUCT_MODULE := true
+# these lines will break builds before 19.1 so make them conditional
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
 LOCAL_USES_LIBRARIES := com.android.location.provider
 LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+endif
+LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
-
