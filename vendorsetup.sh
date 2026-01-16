81a7be48fe008033a99b8b091758187d47abcc5e
SirRGB <sirrgb@proton.me>
2026-01-16 14:52:33 +0100

    chore: drop hard-coded path

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,23 +10,19 @@ get_microg_files() {
     name="$1"
     id="$2"
     src=$(curl -sS https://api.github.com/repos/microg/GmsCore/releases/latest | grep -E "/$id-[0-9]+.apk\"" | cut -d"\"" -f4)
-    curl --location "$src" --output "$name"/"$name".apk
+    curl --location "$src" --output "$(dirname ${BASH_SOURCE[0]})"/"$name"/"$name".apk
 }
 
 get_fdroid_files() {
     src_name="$1"
     trg_name="$2"
-    curl https://f-droid.org/"$src_name".apk --output "$trg_name"/"$trg_name".apk
+    curl https://f-droid.org/"$src_name".apk --output "$(dirname ${BASH_SOURCE[0]})"/"$trg_name"/"$trg_name".apk
 }
-
-cd vendor/partner_gms
 
 get_microg_files GmsCore "com.google.android.gms"
 get_microg_files FakeStore "com.android.vending"
 
 get_fdroid_files F-Droid FDroid
 get_fdroid_files repo/org.fdroid.fdroid.privileged_2130 FDroidPrivilegedExtension
-
-cd ../..
 
 set +e
