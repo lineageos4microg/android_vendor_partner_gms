7b5dc54f7cd0224f3b18a4a3cdbe2f7c357b17ab
SirRGB <sirrgb@proton.me>
2026-01-16 14:32:14 +0100

    chore: add and declare local variables separately
    * SC2155 (warning): Declare and assign separately to avoid masking return values.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,10 +6,11 @@ set -e
 echo "vendor/partner_gms/vendorsetup.sh called"
 
 get_files() {
-    local name="$1"
-    local id="$2"
-    local src=$(curl -sS https://api.github.com/repos/microg/GmsCore/releases/latest | grep -E "/$id-[0-9]+.apk\"" | cut -d"\"" -f4)
-    local destination_apk="$name/$name.apk"
+    local name id src destination_apk
+    name="$1"
+    id="$2"
+    src=$(curl -sS https://api.github.com/repos/microg/GmsCore/releases/latest | grep -E "/$id-[0-9]+.apk\"" | cut -d"\"" -f4)
+    destination_apk="$name/$name.apk"
 
     if [ -f "$destination_apk" ]; then
         echo "$destination_apk exists: not downloading"
