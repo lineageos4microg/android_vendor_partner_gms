92c9cb87056033f08a78e9a9eb5204915b7bc550
Pete Fotheringham <petefoth@e.email>
2026-02-24 17:55:34 +0000

    call new 'create issue' function if update is needed

--- updater/main.py
+++ updater/main.py
@@ -5,39 +5,20 @@ import certificates
 import git
 from sources import ApkRelease, fdroid_recommended_release
 
+def create_update_issue(component: str, new_release: ApkRelease):
+    print('Need to update {} to {}'.format(component, new_release.version_name))
+    print('version_code {}'.format(new_release.version_code))
 
 def update_if_needed(module: str, release: ApkRelease):
     module_dir = path.abspath(path.join(path.dirname(__file__), '..', module))
     with open(path.join(module_dir, '.version_code'), 'r+') as version_code_file:
         version_code = int(version_code_file.read())
         if version_code < release.version_code:
-            print('updating {} to {}'.format(module, release.version_name))
-            apk_filename = path.join(module_dir, '{}.apk'.format(module))
-
-            old_sig = certificates.get_apk_certificate(apk_filename)
-
-            print('downloading {} ...'.format(release.download_url))
-            urllib.request.urlretrieve(release.download_url, apk_filename)
-
-            new_sig = certificates.get_apk_certificate(apk_filename)
-            if old_sig != new_sig:
-                raise Exception('Signature mismatch for {} old sig: {} new sig: {}'.format(module, old_sig, new_sig))
-
-            version_code_file.seek(0)
-            version_code_file.write(str(release.version_code))
-            version_code_file.truncate()
-            version_code_file.close()
-
-            print('Need to update {} to {}'.format(module, release.version_name))
-            return
-
-            print('commit and push...')
-            git.add_commit_push(module_dir, 'Update {} to {}'.format(module, release.version_name))
-
+            create_update_issue(module, release)
         elif version_code > release.version_code:
             print('{} ahead of suggested version ({} > {})'.format(module, version_code, release.version_code))
         elif version_code == release.version_code:
-            print('{} up to date.'.format(module))
+            print('{} up to date, version {}'.format(module, release.version_name))
 
 fdroid_main_repo = 'https://www.f-droid.org/repo'
 fdroid_microg_repo = 'https://microg.org/fdroid/repo'
